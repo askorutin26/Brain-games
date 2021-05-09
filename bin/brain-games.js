e2f4db984fbcdb3237a8a6b40e64bc6dcef8e471
askor26 <askorutin26@gmail.com>
2021-05-09 18:06:54 +0300

    refactored code and fixed syntax with eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
+import getName from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+const name = getName();
+console.log(`Hello, ${name}`);

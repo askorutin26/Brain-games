9f25ab24210bbb43429b4c12c285c72ba767fb2c
askor26 <askorutin26@gmail.com>
2021-05-29 16:09:30 +0300

    refactored random-number-generator

--- src/random-number-generator.js
+++ src/random-number-generator.js
@@ -1,7 +1,7 @@
 const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
+  const innerMin = Math.ceil(min);
+  const innerMax = Math.floor(max);
+  return Math.floor(Math.random() * (innerMax - innerMin + 1)) + innerMin;
+};
 
 export default getRandomNumber;

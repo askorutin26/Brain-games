1315006962434139894086619308daf55c30f08a
askor26 <askorutin26@gmail.com>
2021-05-29 19:14:32 +0300

    refactored brain-progression-Engine

--- src/games/brain-Progression-Engine.js
+++ src/games/brain-Progression-Engine.js
@@ -1,51 +1,42 @@
-import readlineSync from 'readline-sync';
-import greetUser from '../cli.js';
+import getRandomNumber from '../random-number-generator.js';
+import mainEngine from '../index.js'
 
 const playBrainProgression = () => {
   const howToPlay = 'What number is missing in the progression?';
-  const wrongAnswerMessage = 'is wrong answer ;(. Correct answer was';
-  const randomNumberGenerator = () => Math.floor(Math.random() * 100);
-  const randomArrayLength = () => {
-    const min = Math.ceil(5);
-    const max = Math.floor(10);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  const randomDotsPosition = (min, max) => {
-    const innerMin = Math.ceil(min);
-    const innerMax = Math.floor(max);
-    return Math.floor(Math.random() * (innerMax - innerMin) + 1) + innerMin;
-  };
-  const userName = greetUser();
-  console.log(howToPlay);
-
-  for (let i = 0; i < 3; i += 1) {
-    const arrayLength = randomArrayLength();
-    let numberInArray = randomNumberGenerator();
-    const stepOfProgression = randomNumberGenerator();
-
-    const dotsPosition = randomDotsPosition(1, arrayLength);
-
-    let rightAnswer = 0;
-    const arrayOfNumbers = [numberInArray];
-    for (let y = 1; y < arrayLength; y += 1) {
-      numberInArray += stepOfProgression;
-      if (y === dotsPosition) {
+  const getExpression = () => {
+    const arrayLength = getRandomNumber(5,10);
+    const randomDotsPosition = getRandomNumber(0, arrayLength-1);
+    let numberInArray = getRandomNumber(1,100);
+    const arrayOfNumbers = [];
+    const stepOfProgression = getRandomNumber(1,50);
+    for (let y = 0; y < arrayLength; y += 1) {
+        if (y === randomDotsPosition) {
         arrayOfNumbers.push('..');
-        rightAnswer = numberInArray;
-      } else { arrayOfNumbers.push(numberInArray); }
+        } else { arrayOfNumbers.push(numberInArray); }
+      numberInArray += stepOfProgression;
     }
-
-    console.log(`Question: ${arrayOfNumbers.join(' ')}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (+userAnswer !== rightAnswer) {
-      console.log(`'${userAnswer}' ${wrongAnswerMessage} '${rightAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
+    return arrayOfNumbers.join(' ');    
+  };
+  const rightAnswer = (currentExpression) => {
+    const elementsArr = (currentExpression.split(' '));
+    const numberArray=[];
+     const arrayLength = elementsArr.length;
+     let step = 0;
+     let dotsPosition = 0;
+     for (let i = 0; i < arrayLength; i+=1) {
+       if (elementsArr[i]==='..'){dotsPosition = i;
+       numberArray.push('..')}
+       else {numberArray.push (Number(elementsArr[i]));} 
+     }
+     if (dotsPosition+2 < arrayLength-1){step=numberArray[dotsPosition+2]-numberArray[dotsPosition+1];
+     return (String(numberArray[dotsPosition+1]-step))}
+     else {step=numberArray[dotsPosition-1]-numberArray[dotsPosition-2];
+     return (String(numberArray[dotsPosition-1]+step))};
+     
+   }
+  mainEngine(howToPlay, getExpression, rightAnswer);
 };
+  
+  
 
-export default playBrainProgression;
+  export default playBrainProgression;

71d4e980b563791c1e432c75adb1fee948953b57
askor26 <askorutin26@gmail.com>
2021-05-29 16:08:51 +0300

    refactored brain-even-engine

--- src/games/brain-Even-Engine.js
+++ src/games/brain-Even-Engine.js
@@ -1,17 +1,17 @@
 import mainEngine from '../index.js';
-
+import getRandomNumber from '../random-number-generator.js';
 
 const playBrainEven = () => {
-const howToPlay = 'Answer "yes" if the number is even, otherwise answer "no".';
-const rightAnswer = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
+  const howToPlay = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const currentNumber = () => getRandomNumber(1, 100);
+  const rightAnswer = (currentExpression) => {
+    if (currentExpression % 2 === 0) {
+      return 'yes';
+    }
+    return 'no';
+  };
+
+  mainEngine(howToPlay, currentNumber, rightAnswer);
 };
-
-mainEngine(howToPlay,1,100,rightAnswer);
-
-}
 
 export default playBrainEven;

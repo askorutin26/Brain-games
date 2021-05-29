dcc2f6708af2518ee335f52102cf079d8582c7c9
askor26 <askorutin26@gmail.com>
2021-05-29 23:40:02 +0300

    modified brain-prime-engine

--- src/games/brain-Prime-Engine.js
+++ src/games/brain-Prime-Engine.js
@@ -1,34 +1,19 @@
-import readlineSync from 'readline-sync';
-import greetUser from '../cli.js';
+import mainEngine from '../index.js';
+import getRandomNumber from '../random-number-generator.js';
 
 const playBrainPrime = () => {
   const howToPlay = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const wrongAnswerMessage = 'is wrong answer ;(. Correct answer was';
 
-  const randomNumberGenerator = () => Math.floor(Math.random() * 100);
-  const userName = greetUser();
-
-  console.log(howToPlay);
-  for (let i = 0; i < 3; i += 1) {
-    const currentNumber = randomNumberGenerator();
-    const rightAnswer = () => {
-      let counter = 0;
-      for (let y = 1; y <= currentNumber; y += 1) {
-        if (counter > 2) { return 'no'; }
-        if (currentNumber % i === 0) { counter += 1; }
-      }
-      return 'yes';
-    };
-    console.log(`Question: ${currentNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer !== rightAnswer()) {
-      console.log(`'${userAnswer}' ${wrongAnswerMessage} '${rightAnswer()}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
+  const getExpression = () => getRandomNumber(1, 100);
+  const rightAnswer = (currentExpression) => {
+    let counter = 0;
+    for (let y = 1; y <= currentExpression; y += 1) {
+      if (counter > 2) { return 'no'; }
+      if (currentExpression % y === 0) { counter += 1; }
     }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
+    return 'yes';
+  };
+  mainEngine(howToPlay, getExpression, rightAnswer);
 };
 
 export default playBrainPrime;

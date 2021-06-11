9d64592b7a912fedf1ccd8da3f7122cf2edb23c2
askor26 <askorutin26@gmail.com>
2021-06-11 18:59:50 +0300

    refactored prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,9 @@ const isPrime = (number) => {
   if (number === 1) { return false; }
   const squareRoot = Math.floor(Math.sqrt(number));
   for (let i = 2; i <= squareRoot; i += 1) {
-    if (number % i === 0) { return false; }
+    if (number % i === 0) {
+      return false;
+    }
   }
   return true;
 };
